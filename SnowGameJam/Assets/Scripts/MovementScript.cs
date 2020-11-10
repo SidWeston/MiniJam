98bfa3b56b70fea906de9813b0368cbea4be7be0
Leo <49573575+Noctaelum@users.noreply.github.com>
2020-11-10 15:12:16 +0000

    MovementAndIceMats

---
+++ SnowGameJam/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public GameObject Character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d")) {
+            Character.transform.Translate(Time.deltaTime * -5, 0 , 0);
+        }
+
+		if (Input.GetKey("a")) {
+            Character.transform.Translate(Time.deltaTime * 5, 0, 0);
+		}
+    }
+}
