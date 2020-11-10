e39a6a5c3d0c53b5e153c3bcd90456c96ffada5d
SidWeston <60944006+SidWeston@users.noreply.github.com>
2020-11-10 14:47:59 +0000

    Procedural Generation

---
+++ SnowGameJam/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnLocations;
+    public GameObject[] obstaclesToSpawn;
+    public float spawnInterval = 1f;
+
+    private float timeToSpawn = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+   
+    }
+
+    void SpawnObstacles() {
+        int randomLocation = Random.Range(0, spawnLocations.Length);
+        int randomObstacle = Random.Range(0, obstaclesToSpawn.Length);
+
+        for (int i = 0; i < spawnLocations.Length; i++) {
+            if (randomLocation == i) {
+                Instantiate(obstaclesToSpawn[randomObstacle], spawnLocations[i].position, Quaternion.identity);
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeToSpawn) {
+            SpawnObstacles();
+            timeToSpawn = Time.time + spawnInterval;
+        }
+
+
+        
+    }
+}
