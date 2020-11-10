d9d38c9e9d596352cc80fa0b85d56cbe4d437761
Leo <49573575+Noctaelum@users.noreply.github.com>
2020-11-10 16:34:18 +0000

    GameJam

---
+++ SnowGameJam/Assets/Scripts/DistanceTravelled.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceTravelled : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject disTravelledText;
+    public int disTravelled = 0;
+
+    [SerializeField]
+    private Transform checkpoint;
+
+    private float distance;
+
+    [SerializeField]
+    private Text distanceText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        distanceText.text = "Distance travelled: " + distance.ToString("F1") + "m";
+        distance = (checkpoint.transform.position - transform.position).magnitude;
+    }
+}
